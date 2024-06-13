4b0ea36104ddefc4662ccbb0376b5d963607e3ad
RODDY <124875320+RODDY064@users.noreply.github.com>
2024-06-13 16:22:07 +0000

    git wrap in suspense

--- app/(protected)/(dashboard)/dashboard/page.tsx
+++ app/(protected)/(dashboard)/dashboard/page.tsx
@@ -1,8 +1,16 @@
 import { getSignData } from "@/app/server/data"
 import Search from "@/app/ui/search/search"
-import SearchContainer from "@/app/ui/search/searchContainer"
 import Container from "@/app/ui/video/container"
 import { Suspense } from "react"
+
+
+const SearchFallBack = ()=>{
+  return (
+    <div>Loading...</div>
+  )
+}
+
+
 
 export default async function Dashboard() {
 
@@ -11,7 +19,9 @@ export default async function Dashboard() {
   return (
     <div className='w-full h-screen overflow-hidden   md:pl-[13rem] lg:pl-[18rem] md:pr-4 pt-24 md:pt-4 '>
     <div className="w-full h-full relative right-0">
-      <SearchContainer/>
+      <Suspense fallback={<SearchFallBack/>}>
+      <Search/>
+      </Suspense>
       <Container videoData={data}/>
     </div>
     </div>
