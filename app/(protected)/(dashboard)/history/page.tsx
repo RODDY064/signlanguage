9cbe866fdfe563d62ced4b36b59c14773ae1d0ce
RODDY <124875320+RODDY064@users.noreply.github.com>
2024-06-13 18:16:36 +0000

    part 1 push to production

--- app/(protected)/(dashboard)/history/page.tsx
+++ app/(protected)/(dashboard)/history/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function History() {
   return (
-    <div className='w-full h-screen overflow-hidden    md:pl-[13rem] lg:pl-[17rem] md:pr-6 pt-24 md:pt-8 items-center  flex flex-col '>
-      <div className='w-[95%] h-full bg-black'>
+    <div className='w-full h-screen overflow-hidden    md:pl-[13rem] lg:pl-[17rem] md:pr-8 pt-24 md:pt-8 items-center  flex flex-col '>
+      <div className='w-[95%] h-full'>
       <h1 className='text-2xl font-bold'>History</h1>
       </div>
     </div>
