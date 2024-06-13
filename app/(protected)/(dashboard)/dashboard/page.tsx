e5be8fc376cde67d06d994d3a4542775b479f164
RODDY <124875320+RODDY064@users.noreply.github.com>
2024-06-13 16:04:13 +0000

    search wrap in suspense

--- app/(protected)/(dashboard)/dashboard/page.tsx
+++ app/(protected)/(dashboard)/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { getSignData } from "@/app/server/data"
 import Search from "@/app/ui/search/search"
 import Container from "@/app/ui/video/container"
+import { Suspense } from "react"
 
 export default async function Dashboard() {
 
@@ -9,7 +10,9 @@ export default async function Dashboard() {
   return (
     <div className='w-full h-screen overflow-hidden   md:pl-[13rem] lg:pl-[18rem] md:pr-4 pt-24 md:pt-4 '>
     <div className="w-full h-full relative right-0">
-      <Search/>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Search/>
+      </Suspense>
       <Container videoData={data}/>
     </div>
     </div>
