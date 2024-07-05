19a954b34aab91532ed6aad4311407a55ae18fce
RODDY <124875320+RODDY064@users.noreply.github.com>
2024-07-05 03:25:48 +0000

    mobile nav added

---
+++ app/ui/nav/mobileContext.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { createContext ,useContext,useState } from "react";
+
+
+const mobileContext = createContext<{
+ isOpen:boolean,
+ setIsOpen:React.Dispatch<React.SetStateAction<boolean>>
+}| null>(null)
+
+
+function MobileContextProvider({children}:{ children:React.ReactNode}) {
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+  return (
+    <mobileContext.Provider value={{ isOpen, setIsOpen}}>
+      {children}
+    </mobileContext.Provider>
+  )
+}
+
+export default MobileContextProvider;
+
+export function useMobile (){
+  const context = useContext(mobileContext)
+  if(!context){
+    throw new Error('Mobile context must be used in mobile context provider')
+  }
+  return context;
+}
